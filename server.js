21d9b7dab2ffd0047ac8d1130c785cf4879ed8fb
amul-dhungel <applier1234@gmail.com>
2020-11-12 15:59:58 +0545

    "blank"

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const Koa = require('koa')
 const Sequelize = require('sequelize')
-const app = new Koa()
+const json = require('koa-json')
+const KoaRouter = require('koa-router')
 const bodyParser = require('koa-bodyparser')
 const path = require('path')
 
 const PORT = 3000
+const app = new Koa()
+const router = new KoaRouter()
+
+
 
 
 
