be07c6616efa845465e3a06c95f9940be4c59a84
amul-dhungel <applier1234@gmail.com>
2020-11-12 15:56:08 +0545

    "Commit"

---
+++ server.js
@@ -0,0 +1,11 @@
+const Koa = require('koa')
+const Sequelize = require('sequelize')
+const app = new Koa()
+const bodyParser = require('koa-bodyparser')
+const path = require('path')
+
+const PORT = 3000
+
+
+
+
