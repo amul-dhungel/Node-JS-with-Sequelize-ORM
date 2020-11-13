c1a457855d72212b46ecc3eb1afa8768ccf3a327
amul-dhungel <applier1234@gmail.com>
2020-11-13 12:08:53 +0545

    "Database connection"

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const db = require('./config/database')
 
 const app = express()
 const PORT = process.env.PORT || 5000
+
+// Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 // Test the connection of DB
 db.authenticate()
     .then(() => console.log("Database connected..."))
